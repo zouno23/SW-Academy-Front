8e413043ed9bebcbcf657306f195a79c71c2a47b
saif <sabbaghsaif138@gmail.com>
2024-02-28 16:26:15 +0100

    aziz ?

--- src/app/[Role]/dashboard/Cards/DashboardCards.tsx
+++ src/app/[Role]/dashboard/Cards/DashboardCards.tsx
@@ -5,9 +5,9 @@ async function DashboardCards() {
     const {error,response} = await GetCardsNumbers()
     if(error) throw error
     const items =[
-        {index:1, title:"Total Courses",stats:response.Result.CoursePacks},
-        {index:2,title:"Complete lessons" ,stats:response.Result.Lessons},
-        {index:3,title:"Achieved Certificates",stats:response.Result.Certificates}
+        {index:1, title:"Total Courses",stats:response?.Result.CoursePacks},
+        {index:2,title:"Complete lessons" ,stats:response?.Result.Lessons},
+        {index:3,title:"Achieved Certificates",stats:response?.Result.Certificates}
     ]
     return ( <div className='flex justify-between gap-8'>
     {items.map((index )=>
